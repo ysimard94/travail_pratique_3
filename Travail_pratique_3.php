8a666898d108521074444e323144ea28f15de7b7
Yannick Simard <95039695+ysimard94@users.noreply.github.com>
2022-05-03 17:57:46 -0400

    Ajout de code pour création de la table de l'heure

--- Travail_pratique_3.php
+++ Travail_pratique_3.php
@@ -1,9 +1,34 @@
+<head>
+    <title>Travail Pratique 3</title>
+</head>
 <html>
     <body>
         <h1>Travail pratique 3</h1>
         <?php
             date_default_timezone_set('America/Toronto');
             $timezone = date_default_timezone_get();
+
+            $time = date('h:i');
+
+            echo $time
         ?>
+
+        <table>
+        <?php 
+            echo "<table border='1'><br />";
+
+            $heures = range(1, 12);
+            $minutes = range(1, 59);
+            
+            foreach($heures as $heure){
+                echo "<tr><th>" . $heure . "</th>";
+                foreach($minutes as $minute){
+                    echo "<td>" . $minute . "</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+
+        </table>
     </body>
 </html>
