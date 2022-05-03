a29b89453be3268f7c6974ee4f4f44a0818215de
Yannick Simard <95039695+ysimard94@users.noreply.github.com>
2022-05-03 16:46:53 -0400

    Début du travail

---
+++ Travail_pratique_3.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>Travail pratique 3</h1>
+        <?php
+            date_default_timezone_set('America/Toronto');
+            $timezone = date_default_timezone_get();
+        ?>
+    </body>
+</html>
